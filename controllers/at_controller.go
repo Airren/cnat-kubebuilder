77591c76538bb942b81b033e58991d1a21bb0ed8
Airren <renqiqiang@outlook.com>
2022-07-04 16:04:32 +0000

    tmp storeage

--- controllers/at_controller.go
+++ controllers/at_controller.go
@@ -18,11 +18,19 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/pkg/apis/clientauthentication/install"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	cnatv1alpha1 "www.github.com/airren/cnat-kubebuilder/api/v1alpha1"
 )
@@ -50,6 +58,90 @@ func (r *AtReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Re
 	_ = log.FromContext(ctx)
 
 	// TODO(user): your logic here
+
+	reqLogger := log.Log.WithValues("namespace", req.Namespace, "at", req.Name)
+	reqLogger.Info("=== Reconciling At")
+
+	// Fetch the At instance
+	instance := &cnatv1alpha1.At{}
+	err := r.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile
+			// request-return and don't requeue
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object-requeue the request:
+		return reconcile.Result{}, err
+	}
+
+	// If no phase set, default to pending (the initial phase)
+	if instance.Status.Phase == "" {
+		instance.Status.Phase = cnatv1alpha1.PhasePending
+	}
+
+	// Now let's make the main case distinction: implementing the state diagram
+	// PENDING -> RUNNING -> DONE
+	switch instance.Status.Phase {
+	case cnatv1alpha1.PhasePending:
+		reqLogger.Info("Phase: PENDING")
+		// As long as we haven't executed the command yet, we need to check if 
+		// it's already time to act
+		reqLogger.Info("Checking schedule","Target", instance.Spec.Schedule)
+		// Check if it's already time to execute the command with a tolerance of 
+		// 2 second
+		d, err := timeUtilSchedule(instance.Spec.Schedule)
+		if err != nil{
+			reqLogger.Error(err, "Schedule parsing failure")
+			// Error reading the schedule. Wait until it is fixed.
+			return reconcile.Result{},err
+		}
+		reqLogger.Info("Schedule parsing done","Result","diff",fmt.Sprintf("%v",d))
+
+		if d >0{
+			// Not yet time to execute the command, wait until the schedule time
+			return reconcile.Result{RequeueAfter: d},nil
+		}
+
+		reqLogger.Info("It's time!", "Ready to execute", instance.Spec.Command)
+		instance.Status.Phase = cnatv1alpha1.PhaseRunning
+
+	case cnatv1alpha1.PhaseRunning:
+		reqLogger.Info("Phase: RUNNING")
+		pod := newPodForCR(instance)
+		// Set At instance as the owner and controller
+		err := controllerutil.SetControllerReference(instance, pod, r.Scheme) 
+		if err != nil{
+			// requeue with error
+			return reconcile.Result{},err
+		}
+		found := &corev1.Pod{}
+		nsName := types.NamespacedName{
+			Name: pod.Name,
+			Namespace: pod.Namespace,
+		}
+		err := r.Get(context.TODO(), nsName, found)
+		// Try to see if the pod already exist and if not
+		// (which we expect) then create a noe-shot pod as per spec
+		if err!= nil && errors.IsNotFound(err){
+			err = r.Create(context.TODO(), pod)
+			if err != nil{
+				// requeue with error
+				return reconcile.Result{},err
+			}
+		} else if err != nil{
+			// requeue with error
+			return reconcile.Result{},err
+		} else if found.Status.Phase == corev1.PodFaild || found.Status.Phase == corev1.PodSucceed{
+			reqLogger.Info("Container terminated","reason", found.Status.Reason,
+		"message", found.Status.Message)
+		} else{
+			// 
+		}
+
+
+
+		 
 
 	return ctrl.Result{}, nil
 }
